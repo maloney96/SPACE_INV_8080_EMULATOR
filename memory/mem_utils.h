406278e377b48bf734af634aad8741dec473564f
Noah Freeman <114368114+librepax@users.noreply.github.com>
2024-10-30 17:57:52 -0700

    Update mem_utils.h

--- memory/mem_utils.h
+++ memory/mem_utils.h
@@ -1,3 +1,7 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #ifndef MEM_UTILS_H
 #define MEM_UTILS_H
 
@@ -8,3 +12,7 @@
 void print_ram_contents(uint8_t *ram, size_t size, const char *filename);
 
 #endif // MEM_UTILS_H
+
+#ifdef __cplusplus
+}
+#endif
