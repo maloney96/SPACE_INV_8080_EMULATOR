f04bf87bd33ce25c479e5fdc9d75c304f439de6e
colincummins <all.our.august.sundays@gmail.com>
2024-11-20 17:16:15 -0800

    Current timepoint should be when opcode begins execution

--- emulator/emulatorWrapper.cpp
+++ emulator/emulatorWrapper.cpp
@@ -150,7 +150,7 @@ void EmulatorWrapper::runCycle() {
         EmulatorWrapper::handleIN(opcode);
         }
         cycles_used = emulate_8080cpu(&state);
-        previous_cycle_time = std::chrono::high_resolution_clock::now();
+        previous_cycle_time = current_timepoint;
     }
 
     // Handle interrupts
